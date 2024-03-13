740f703a7a2988544507b69e5de93bf786938c4e
Sese Schneider <sese.tailor@gmx.net>
2024-03-13 16:25:09 +0100

    Reenable android support through compiled libraries

--- lib/framework/dive_computer_ffi.dart
+++ lib/framework/dive_computer_ffi.dart
@@ -35,14 +35,13 @@ class DiveComputerFfi {
       );
     });
 
-    const String libName = 'dive_computer';
     String fileName;
     if (Platform.isWindows) {
       fileName = 'libdivecomputer-0.dll';
     } else if (Platform.isAndroid) {
-      fileName = 'lib$libName.so';
+      fileName = 'libdivecomputer.so';
     } else if (Platform.isMacOS || Platform.isIOS) {
-      fileName = '$libName.framework/$libName';
+      fileName = 'dive_computer.framework/dive_computer';
     } else {
       throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
     }
